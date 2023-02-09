385748b9006eefc0c98c10ace05fd24c14de5597
habos <habos@crimson.ua.edu>
2023-02-09 13:51:26 +0100

    add jest tests and some light cleanup

---
+++ src/components/tests/Dropdown.spec.ts
@@ -0,0 +1,38 @@
+import { DOMWrapper, mount } from '@vue/test-utils';
+import Dropdown from '../Dropdown.vue';
+import { describe, expect, test } from '@jest/globals';
+
+describe('mount dropdown', () => {
+  const dropdownList = ['Option1', 'Option2', 'Option3'];
+  const wrapper = mount(Dropdown, {
+    props: {
+      dropdownList,
+    },
+  });
+
+  const testArr = wrapper.findAll('a').map(function (e, i) {
+    return [e, dropdownList[i]];
+  });
+
+  test('dropdown should have 3 items', () => {
+    expect(wrapper.findAll('a')).toHaveLength(3);
+  });
+
+  test.each(testArr)(
+    'test list text',
+    (a: DOMWrapper<HTMLAnchorElement>, b: string) => {
+      expect(a.text()).toContain(b);
+    }
+  );
+
+  wrapper.find('a').trigger('click');
+
+  test('emits when clicked', () => {
+    expect(wrapper.emitted()).toHaveProperty('optionSelected');
+    const optionEvent = wrapper.emitted('optionSelected');
+    if (optionEvent !== undefined) {
+      expect(optionEvent).toHaveLength(1);
+      expect(optionEvent[0]).toEqual(['Option1']);
+    }
+  });
+});
---
+++ src/components/tests/Toggle.spec.ts
@@ -0,0 +1,37 @@
+import { DOMWrapper, mount } from '@vue/test-utils';
+import Toggle from '../Toggle.vue';
+import { describe, expect, test } from '@jest/globals';
+
+describe('mount toggle (disabled false)', () => {
+  const wrapper = mount(Toggle, {
+    props: {
+      disabled: false,
+    },
+  });
+
+  test('check if input is disabled', () => {
+    expect(wrapper.find('input').attributes()).not.toHaveProperty('disabled');
+  });
+
+  test('emits when clicked', () => {
+    wrapper.find('input').trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('toggle');
+  });
+});
+
+describe('mount toggle (disabled true)', () => {
+  const wrapper = mount(Toggle, {
+    props: {
+      disabled: true,
+    },
+  });
+
+  test('check if input is disabled', () => {
+    expect(wrapper.find('input').attributes()).toHaveProperty('disabled');
+  });
+
+  test('does not emit when clicked', () => {
+    wrapper.find('input').trigger('click');
+    expect(wrapper.emitted()).not.toHaveProperty('toggle');
+  });
+});
